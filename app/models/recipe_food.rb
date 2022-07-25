8031e55181777852c10b2638ca3418d7bd1e67e0
Alexr16 <j.alex-1996@hotmail.com>
2022-07-25 12:30:59 -0500

    Add relationships and validations

--- app/models/recipe_food.rb
+++ app/models/recipe_food.rb
@@ -1,2 +1,5 @@
 class RecipeFood < ApplicationRecord
+    belongs_to :recipe, :food
+
+    validates :quantity, presence: true, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+    has_many :foods, dependent: :destroy
+    has_many :recipes, dependent: :destroy
+
+    validates :name, presence: true, length: { maximum: 15 }
 end
