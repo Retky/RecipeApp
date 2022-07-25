36d86fd3c6fffbdd77de4d12d6f9a8b1f49a0c6f
Alexr16 <j.alex-1996@hotmail.com>
2022-07-25 12:56:27 -0500

    Test database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+# Users
+user1 = User.create(name: "Juan")
+
+# Foods
+
+food1 = Food.create(name: "apple", measurement_unit: "pz", price: 1.0, quantity: 1, user: user1)
+
+# Recipes
+
+recipe1 = Recipe.create(name: "apple pie", preparation_time: 1.5, cooking_time: 1.0, description: "apple pie", public: true, user: user1)
+# RecipeFoods
+
+recipe_food1 = RecipeFood.create(food: food1, recipe: recipe1, quantity: 1)
