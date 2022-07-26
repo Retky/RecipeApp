31005b132322eac5259514ff45a01a265fdb1e42
Alexr16 <j.alex-1996@hotmail.com>
2022-07-26 18:00:34 -0500

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,10 +11,13 @@ Rails.application.routes.draw do
 
   resources :users, only: [:new, :create] do
     resources :foods, only: [:index, :new, :create, :destroy]
-    resources :recipes, only: [:index, :new, :create, :destroy, :show]
+    resources :recipes, only: [:index, :new, :create, :destroy, :show] do
+      resources :recipe_foods, only: [:new, :create, :destroy]
+    end
     resources :public_recipes, only: [:index]
     resources :shopping_lists, only: [:index]
   end
 
   resources :foods, only: [:new, :create, :destroy]
+  resources :recipe_foods, only: [:edit, :update]
 end
