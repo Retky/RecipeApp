9bb634b903e6103a9b0bfed34a95db2c33e97255
Alexr16 <j.alex-1996@hotmail.com>
2022-07-27 14:08:42 -0500

    Create add ingredient methods

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,23 +1,24 @@
 class RecipeFoodsController < ApplicationController
   def new
-    # @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = RecipeFood.new
     # render :new, locals: { recipe_food: @recipe_food, recipe: @recipe }
   end
 
   def create
     @user = current_user
-    @recipe = Recipe.find(params[:recipe_id])
-    @recipe_food = @recipe.recipe_food.new(recipe_food_params)
-    @recipe_food.recipe_id = @recipe.id
-    puts @recipe_food.inspec
+    @food = Food.find_by(id: recipe_food_params[:food_id])
+    # @recipe = Recipe.find_by(id: recipe_food_params[:recipe_id])
+    # @recipe = Recipe.find_by(id: params[:recipe_id])
+    @recipe_food = RecipeFood.new(food_id: @food, quantity: recipe_food_params[:quantity], recipe_id: recipe_food_params[:recipe_id])
+    # @recipe_food.recipe_id = @recipe.id
     if @recipe_food.save
-      flash[:notice] = 'Recipe successfully created!'
-      redirect_to user_recipe_path(@user.id, @recipe.id)
+      flash[:notice] = 'Recipe food successfully created!'
+      redirect_to user_recipe_path(current_user, @recipe)
     else
       render :new
     end
   end
+ 
 
   def destroy
     @recipe_food = RecipeFood.find(params[:id])
@@ -30,8 +31,8 @@ class RecipeFoodsController < ApplicationController
   end
 
   def update
-    @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = RecipeFood.find(params[:id])
+    @recipe = Recipe.find(@recipe_food.recipe_id)
     @recipe_food.update(quantity: params[:recipe_food][:quantity])
     redirect_to user_recipe_path(current_user, @recipe)
   end
@@ -39,7 +40,7 @@ class RecipeFoodsController < ApplicationController
   private
 
   def recipe_food_params
-    params.require(:recipe_food).permit(:food_id, :quantity)
+    params.require(:recipe_food).permit(:food_id, :quantity, :recipe_id)
   end
 
   def recipe_food_modify_params
