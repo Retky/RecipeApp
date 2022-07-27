aa598e0bad51ffb2e81d9fcb8c5547ce0ebae761
Alexr16 <j.alex-1996@hotmail.com>
2022-07-27 17:44:49 -0500

    Refactor create ingredient method

--- app/controllers/recipe_foods_controller.rb
+++ app/controllers/recipe_foods_controller.rb
@@ -1,36 +1,42 @@
 class RecipeFoodsController < ApplicationController
   def new
     @recipe_food = RecipeFood.new
-    # render :new, locals: { recipe_food: @recipe_food, recipe: @recipe }
   end
 
   def create
+    if current_user
     @user = current_user
-    @food = Food.find_by(id: recipe_food_params[:food_id])
-    # @recipe = Recipe.find_by(id: recipe_food_params[:recipe_id])
-    # @recipe = Recipe.find_by(id: params[:recipe_id])
-    @recipe_food = RecipeFood.new(food_id: @food, quantity: recipe_food_params[:quantity],
-                                  recipe_id: recipe_food_params[:recipe_id])
-    # @recipe_food.recipe_id = @recipe.id
-    if @recipe_food.save
+    @recipe = @user.recipes.find(params[:recipe_id])
+    @food = @user.foods.find_by(id: recipe_food_params[:food_id])
+    @recipe_food = @recipe.recipe_foods.new(recipe_food_params)
+    @recipe_food.recipe_id = @recipe.id
+    @recipe_food.food_id = @food.id
+    if @recipe_food.save && user_signed_in?
       flash[:notice] = 'Recipe food successfully created!'
       redirect_to user_recipe_path(current_user, @recipe)
     else
-      render :new
+      flash[:notice] = 'Recipe food not created!'
+      render :new, status: :unprocessable_entity
     end
+  end
   end
 
   def destroy
+    @user = current_user
+    @recipe = Recipe.find(params[:recipe_id])
     @recipe_food = RecipeFood.find(params[:id])
     @recipe_food.destroy
-    #   redirect_to user_recipe_path(@user.id, @recipe.id)
+    redirect_to user_recipe_path(@user.id, @recipe.id)
   end
 
   def edit
     @recipe_food = RecipeFood.find(params[:id])
   end
 
+
+
   def update
+    @user = current_user
     @recipe_food = RecipeFood.find(params[:id])
     @recipe = Recipe.find(@recipe_food.recipe_id)
     @recipe_food.update(quantity: params[:recipe_food][:quantity])
@@ -40,7 +46,7 @@ class RecipeFoodsController < ApplicationController
   private
 
   def recipe_food_params
-    params.require(:recipe_food).permit(:food_id, :quantity, :recipe_id)
+    params.require(:recipe_food).permit(:food_id, :quantity)
   end
 
   def recipe_food_modify_params
